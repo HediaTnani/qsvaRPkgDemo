567abd76bf6c0f00d92b3bd7bd649c2599a5a2d7
HediaTnani <hediatnani0@gmail.com>
2023-07-26 06:43:31 -0400

    Style vignette

--- dev/03_core_files.R
+++ dev/03_core_files.R
@@ -78,9 +78,9 @@ biocthis::use_bioc_vignette(pkg, "Differential Expression Analysis using Limma a
 
 ## Add a Bioconductor-friendly GitHub actions workflow to check your package
 biocthis::use_bioc_github_action(
-  pkgdown = TRUE,
-  testthat = TRUE,
-  docker = TRUE
+    pkgdown = TRUE,
+    testthat = TRUE,
+    docker = TRUE
 )
 ## You will need to go to https://github.com//settings/actions
 ## to and enable:
